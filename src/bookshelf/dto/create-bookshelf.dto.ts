4365926567f3ad1d86a771b284fbd79b63adbc7b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-06 23:43:46 +0700

    add property and validation pipe in create bookshelf dto

--- src/bookshelf/dto/create-bookshelf.dto.ts
+++ src/bookshelf/dto/create-bookshelf.dto.ts
@@ -1,15 +1,23 @@
-import { IsString, IsDateString, IsBoolean } from "class-validator";
+import { IsString, IsDateString, IsBoolean, IsNotEmpty, IsISO8601, IsOptional } from "class-validator";
 
 export class CreateBookshelfDto {
     @IsString()
+    @IsNotEmpty()
     Judul: string;
 
     @IsString()
+    @IsNotEmpty()
     Penulis: string;
 
-    @IsDateString()
+    // @IsDateString()
+    @IsNotEmpty()
     TanggalTerbit: Date;
 
     @IsBoolean()
+    @IsNotEmpty()
     IsCompleted: Boolean;
+
+    @IsOptional()
+    HalamanTerakhir?: number;
+
 }
