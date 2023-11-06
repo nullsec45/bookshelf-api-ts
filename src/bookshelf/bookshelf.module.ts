cceb4a7903336f01860a5bc36dc122f310c7a644
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-06 23:44:20 +0700

    import prisma module to bookshelf module

--- src/bookshelf/bookshelf.module.ts
+++ src/bookshelf/bookshelf.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookshelfService } from './bookshelf.service';
 import { BookshelfController } from './bookshelf.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [BookshelfController],
   providers: [BookshelfService],
 })
-export class BookshelfModule {}
+export class BookshelfModule { }
