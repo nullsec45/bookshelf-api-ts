ee5638e868766ffcbb33f7a838b4197c8ed1fde1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-06 23:45:22 +0700

    import prisma module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { BookshelfController } from './bookshelf/bookshelf.controller';
 import { BookshelfService } from './bookshelf/bookshelf.service';
 import { BookshelfModule } from './bookshelf/bookshelf.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BookshelfModule],
+  imports: [BookshelfModule, PrismaModule],
   controllers: [AppController, BookshelfController],
   providers: [AppService, BookshelfService],
 })
