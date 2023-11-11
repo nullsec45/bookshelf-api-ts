05e2f48f2456c14240f9e6de75b99b2475f7c211
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-11 18:38:30 +0700

    m:import auth module, users module, jwt service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { BookshelfController } from './bookshelf/bookshelf.controller';
 import { BookshelfService } from './bookshelf/bookshelf.service';
 import { BookshelfModule } from './bookshelf/bookshelf.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { JwtService } from '@nestjs/jwt/dist';
+
 
 @Module({
-  imports: [BookshelfModule, PrismaModule],
+  imports: [BookshelfModule, PrismaModule, AuthModule, UsersModule],
   controllers: [AppController, BookshelfController],
-  providers: [AppService, BookshelfService],
+  providers: [AppService, BookshelfService, JwtService],
 })
-export class AppModule {}
+export class AppModule { }
