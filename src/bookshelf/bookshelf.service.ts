9132a8e6683e581ca99f82791f496b02c7f72baf
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-01-07 14:00:45 +0700

    where user id to method service

--- src/bookshelf/bookshelf.service.ts
+++ src/bookshelf/bookshelf.service.ts
@@ -10,13 +10,14 @@ export class BookshelfService {
 
   }
   async create(createBookshelfDto: CreateBookshelfDto) {
-    const { tanggal_terbit, ...bookShelfs } = createBookshelfDto;
+    const { tanggal_terbit, user_id, ...bookShelfs } = createBookshelfDto;
     const iso8601Date = new Date(tanggal_terbit).toISOString();
 
 
     const createdData = await this.prisma.bookshelfs.create({
       data: {
         ...bookShelfs,
+        user_id,
         tanggal_terbit: iso8601Date,
         is_completed: createBookshelfDto.is_completed.valueOf()
       }
@@ -28,8 +29,12 @@ export class BookshelfService {
     }
   }
 
-  async findAll() {
-    const dataBookshelf = await this.prisma.bookshelfs.findMany({});
+  async findAll(userId: number) {
+    const dataBookshelf = await this.prisma.bookshelfs.findMany({
+      where: {
+        user_id: userId
+      }
+    });
 
     return {
       statusCode: 200,
@@ -37,10 +42,11 @@ export class BookshelfService {
     }
   }
 
-  async findOne(id: number) {
+  async findOne(id: number, userId: number) {
     const dataBookshelf = await this.prisma.bookshelfs.findFirst({
       where: {
         id: id,
+        user_id: userId
       }
     });
 
@@ -48,16 +54,16 @@ export class BookshelfService {
       return {
         statusCode: 404,
         message: "Book Not Found"
-        // data: dataBookshelf
       }
     }
+
     return {
       statusCode: 200,
       data: dataBookshelf
     }
   }
 
-  async update(id: number, updateBookshelfDto: UpdateBookshelfDto) {
+  async update(id: number, userId: number, updateBookshelfDto: UpdateBookshelfDto) {
     const { tanggal_terbit, ...bookShelf } = updateBookshelfDto;
     const iso8601Date = new Date(tanggal_terbit).toISOString();
 
@@ -69,6 +75,7 @@ export class BookshelfService {
       },
       where: {
         id: id,
+        user_id: userId
       }
     })
 
@@ -78,10 +85,11 @@ export class BookshelfService {
     };
   }
 
-  async remove(id: number) {
+  async remove(id: number, user_id: number) {
     const bookShelf = await this.prisma.bookshelfs.delete({
       where: {
         id: id,
+        user_id
       },
     });
     return {
